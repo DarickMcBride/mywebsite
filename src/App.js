f1e4428752d99c8a715aa79a88b2165976c7eb28
Darick McBride <darickmcbride@me.com>
2019-03-12 22:06:41 -0700

    add header complete

--- src/App.js
+++ src/App.js
@@ -6,14 +6,46 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <NavBar/>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="Large-App-logo" alt="logo" />
           <p>
             Welcome! This site is under construction and will be up shortly.
           </p>
         </header>
       </div>
     );
+  }
+}
+
+class NavBar extends App{
+  render(){
+    return(
+
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#"> 
+          <img src={logo} className="App-logo" alt="logo" />
+        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarColor02">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About</a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="text" placeholder="Search"></input>
+            <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    )
   }
 }
 

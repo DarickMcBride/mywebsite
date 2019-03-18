a0300dbe850cad7e654e817400fc22ff4c7546c7
Darick McBride <darickmcbride@me.com>
2019-03-18 13:28:41 -0700

    took out linkedin badge

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,12 +5,9 @@ class HomePage extends Component {
   render() {
     return (
       <div className="Home-top">
-        <div className="Home-text">
           <p>
             Welcome! This site is under construction and will be up shortly.
           </p>
-        </div>
-        <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="dark" data-vanity="darickmcbride"><a class="LI-simple-link" href='https://www.linkedin.com/in/darickmcbride?trk=profile-badge'>Darick McBride</a></div>
       </div>
     )
   }

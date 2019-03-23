b3fd1b5e6d80032a7f54b0ca97375957a9d56ac3
Darick McBride <darickmcbride@me.com>
2019-03-23 09:51:31 -0700

    took out bootstrap

--- src/ContactPage.jsx
+++ src/ContactPage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg'
+
 
 class ContactPage extends Component {
   render() {
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
-
 import './custom.scss'
 import './index.css';
 import App from './App';
